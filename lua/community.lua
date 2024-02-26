823707a8d556055ec834e24e80f0516a36e7b6b2
Minebomber <markmc909@gmail.com>
2024-02-26 00:52:30 -0800

    Rebase config

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,6 +6,15 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   -- TODO: Remove branch v4 on release
   { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
   -- import/override with your plugins folder
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,58 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
 -- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+
+local ftSetupGrp = vim.api.nvim_create_augroup("FileTypeSetup", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "*.vert", "*.frag" },
+  command = [[ set ft=glsl ]],
+  group = ftSetupGrp,
+})
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.json.jbuilder",
+  command = [[ set ft=ruby ]],
+  group = ftSetupGrp,
+})
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = ".env*",
+  command = [[ set ft=sh ]],
+  group = ftSetupGrp,
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "tsconfig.json",
+  command = [[ set ft=jsonc ]],
+  group = ftSetupGrp,
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.h",
+  command = [[ set ft=c ]],
+  group = ftSetupGrp,
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = ".env*",
+  command = [[ set ft=sh ]],
+  group = ftSetupGrp,
+})
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.jlib",
+  command = [[ set ft=javascript ]],
+  group = ftSetupGrp,
+})
+vim.api.nvim_create_autocmd({ "BufReadPre" }, {
+  pattern = "*.asm",
+  command = [[ let g:asmsyntax = "fasm" ]],
+  group = ftSetupGrp,
+})
