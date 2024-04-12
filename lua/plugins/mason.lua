2f7866512a2cd66e4634f7b0412cb830885a8253
Minebomber <markmc909@gmail.com>
2024-04-12 09:40:36 -0700

    add omnisharp_mono as LSP

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,6 +10,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         -- add more arguments for adding more language servers
+        "omnisharp_mono",
       })
     end,
   },
